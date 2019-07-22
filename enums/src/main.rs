0689436b0b61f2ad3d12dfcdc10498858b41f56d
Tamil Selvan <tamilps2@gmail.com>
2019-07-22 17:07:58 +0530

    enums exercise

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -18,15 +18,53 @@
 //     println!("ip is {}", ip.value);
 // }
 
+// enum with value
+// fn main () {
+//     enum IpAddrStr {
+//         V4(String),
+//         V6(String)
+//     };
 
-fn main () {
-    enum IpAddrStr {
-        V4(String),
-        V6(String)
-    };
+//     enum IpAddrDiffTypes {
+//         V4(u8, u8, u8, u8),
+//         v6(String)
+//     };
+// }
 
-    enum IpAddrDiffTypes {
-        V4(u8, u8, u8, u8),
-        v6(String)
-    };
+// enum with match expression
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+    BitCoin
+}
+
+fn main() {
+    println!("value in cents is {}", value_in_cents(Coin::Penny));
+    println!("value in cents is {}", value_in_cents(Coin::Nickel));
+    println!("value in cents is {}", value_in_cents(Coin::Dime));
+    println!("value in cents is {}", value_in_cents(Coin::Quarter));
+    println!("value in cents is {}", value_in_cents(Coin::BitCoin));
+
+
+    // if let 
+    if let Coin::Dime = Coin::Quarter {
+        println!("It's a match");
+    } else {
+        println!("It's not a match");
+    }
+}
+
+fn value_in_cents(coin: Coin) -> u32 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+        _ => {
+            println!("Not a valid coin");
+            0
+        }
+    }
 }
