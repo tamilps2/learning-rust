542706d7160163ab0f60d7689d1d117747c3664a
Tamil Selvan <tamilps2@gmail.com>
2019-07-23 16:53:01 +0530

    modules

---
+++ communicator/src/network/mod.rs
@@ -0,0 +1,6 @@
+pub mod server;
+
+pub fn connect() {
+
+}
+
---
+++ mod-privacy/src/lib.rs
@@ -0,0 +1,21 @@
+mod outermost {
+    pub fn middle_function() {
+        inside::inner_function();
+        middle_secret_function();
+    }
+
+    fn middle_secret_function() {}
+
+    mod inside {
+        pub fn inner_function() {}
+
+        fn secret_function() {}
+    }
+}
+
+fn try_me() {
+    outermost::middle_function();
+    // outermost::middle_secret_function();
+    // outermost::inside::inner_function();
+    // outermost::inside::secret_function();
+}
