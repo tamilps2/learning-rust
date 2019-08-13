017e9f4e1870aed3f73b0a4d2bf0f6b6055360aa
Tamil Selvan <tamilps2@gmail.com>
2019-08-13 17:05:24 +0530

    Traits

--- traits/src/main.rs
+++ traits/src/main.rs
@@ -1,3 +1,23 @@
+mod news;
+// mod tweets;
+
+pub trait Summary {
+    fn summary(&self) -> String;
+}
+
+impl Summary for news::NewsArticle {
+    fn summary(&self) -> String {
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let article = news::NewsArticle {
+        headline: String::from("This is a test line"),
+        location: String::from("TN"),
+        author: String::from("Mineu"),
+        content: String::from("This is a lorem possum"),
+    };
+
+    println!("{}", article.summary());
 }
